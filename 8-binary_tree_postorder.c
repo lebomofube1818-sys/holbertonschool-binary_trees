2f2b78f3261c51169958b389f0c415040c30637f
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2025-11-04 16:12:48 +0200

    Fix Task 8: Betty-compliant post-order traversal (line length fix)

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,7 +5,8 @@
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  *
- * Description: Traverse the binary tree in post-order (left, right, root)
+ * Description: Traverses the left subtree, then the right subtree,
+ * then the node. If tree or func is NULL, the function does nothing.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
